14b722cd097a93a064b85efe6ef23c6cde1b6a79
Vidar S. Ramdal <vramdal@gmail.com>
2017-10-12 23:34:30 +0200

    Fikser

--- index.js
+++ index.js
@@ -6,20 +6,20 @@ var events = require("events");
 
 let serverName = process.env.SERVER_NAME;
 
-let proxyserver = new events.EventEmitter();
+let requestHandler = function helloGET (request, response) {
 
-/*  helper function: emit event or run fallback action  */
-let emitOrRun = function (eventName, callback) {
-    if (!proxyserver.listeners(eventName).length)
-        callback();
-    else {
-        let args = Array.prototype.slice.call(arguments, 2);
-        args.unshift(eventName);
-        proxyserver.emit.apply(proxyserver, args);
-    }
-};
+    let eventEmitter = new events.EventEmitter();
 
-exports.redirectGoogleAppsCall = function helloGET (request, response) {
+    /*  helper function: emit event or run fallback action  */
+    let emitOrRun = function (eventName, callback) {
+        if (!eventEmitter.listeners(eventName).length)
+            callback();
+        else {
+            let args = Array.prototype.slice.call(arguments, 2);
+            args.unshift(eventName);
+            eventEmitter.emit.apply(eventEmitter, args);
+        }
+    };
 
     /*  determine connection id  */
     let cid = request.connection.remoteAddress + ":" + request.connection.remotePort;
@@ -52,13 +52,13 @@ exports.redirectGoogleAppsCall = function helloGET (request, response) {
         followRedirect: false,
         encoding:       null
     };
-/*
-    if (proxy !== "") {
-        let hostname = url.parse(remoteRequest.url).hostname;
-        if (hostname !== "localhost" && hostname !== "127.0.0.1")
-            remoteRequest.proxy = proxy;
-    }
-*/
+    /*
+     if (proxy !== "") {
+     let hostname = url.parse(remoteRequest.url).hostname;
+     if (hostname !== "localhost" && hostname !== "127.0.0.1")
+     remoteRequest.proxy = proxy;
+     }
+     */
 
     /*  helper function for fixing the upper/lower cases of headers  */
     let fixHeaderCase = function (headers) {
@@ -82,7 +82,7 @@ exports.redirectGoogleAppsCall = function helloGET (request, response) {
             req(remoteRequest, function (error, remoteResponse, remoteResponseBody) {
                 /*  perform the HTTP client response  */
                 if (error) {
-                    proxyserver.emit("http-error", cid, error, request, response);
+                    eventEmitter.emit("http-error", cid, error, request, response);
                     response.writeHead(400, {});
                     response.end();
                 }
@@ -99,7 +99,7 @@ exports.redirectGoogleAppsCall = function helloGET (request, response) {
             });
         }
         catch (error) {
-            proxyserver.emit("http-error", cid, error, request, response);
+            eventEmitter.emit("http-error", cid, error, request, response);
             response.writeHead(400, {});
             response.end();
         }
@@ -118,6 +118,7 @@ exports.redirectGoogleAppsCall = function helloGET (request, response) {
 
 
 };
+exports.redirectGoogleAppsCall = requestHandler;
 
 
 /*
@@ -133,3 +134,75 @@ proxy.on("http-intercept-response", function (cid, request, response, remoteResp
     performResponse(remoteResponse, remoteResponseBody);
 });
 */
+
+exports.createProxyServer = function (opts) {
+    /*  prepare settings  */
+    opts = opts || {};
+    var serverHost   = opts.host               || "127.0.0.1";
+    var serverPort   = parseInt(opts.port, 10) || 3128;
+    var serverName   = opts.servername         || os.hostname();
+    var proxy        = opts.proxy              || "";
+
+    /*  determine service identifier  */
+    var id = opts.id;
+    if (typeof id === "undefined" || (id + "").match(/^[a-zA-Z0-9_-]+\/[0-9](?:\.[0-9])*$/) === null) {
+        /* global __dirname: true */
+        var pjson = JSON.parse(fs.readFileSync(path.join(__dirname, "package.json"), "utf8"));
+        id = pjson.name + "/" + pjson.version;
+    }
+
+    /*  create event emitting proxy object  */
+    var proxyserver = new events.EventEmitter();
+
+    /*  helper function: emit event or run fallback action  */
+    var emitOrRun = function (eventName, callback) {
+        if (!proxyserver.listeners(eventName).length)
+            callback();
+        else {
+            var args = Array.prototype.slice.call(arguments, 2);
+            args.unshift(eventName);
+            proxyserver.emit.apply(proxyserver, args);
+        }
+    };
+
+    /*  create HTTP server  */
+    var httpServer = http.createServer(requestHandler);
+
+
+
+    /*  react upon HTTP server events  */
+    httpServer.on("connection", function (socket) {
+        var cid = socket.remoteAddress + ":" + socket.remotePort;
+        proxyserver.emit("connection-open", cid, socket);
+        socket.on("close", function (had_error) {
+            proxyserver.emit("connection-close", cid, socket, had_error);
+        });
+        socket.on("error", function (error) {
+            proxyserver.emit("connection-error", cid, socket, error);
+        });
+    });
+    httpServer.on("clientError", function () {
+        /*  already handled above on socket directly  */
+    });
+    httpServer.on("request", function (request, response) {
+        if (typeof request.connection.remoteAddress === "undefined" ||
+            typeof request.connection.remotePort    === "undefined"   )
+            return;
+        var cid = request.connection.remoteAddress + ":" + request.connection.remotePort;
+        proxyserver.emit("http-request", cid, request, response);
+    });
+    httpServer.on("connect", function (request, socket) {
+        var cid = request.connection.remoteAddress + ":" + request.connection.remotePort;
+        proxyserver.emit("http-error", cid, "CONNECT method not supported", request, socket);
+    });
+    httpServer.on("upgrade", function (request, socket) {
+        var cid = request.connection.remoteAddress + ":" + request.connection.remotePort;
+        proxyserver.emit("http-error", cid, "protocol upgrade not supported", request, socket);
+    });
+
+    /*  listen for connections  */
+    httpServer.listen(serverPort, serverHost);
+
+    return proxyserver;
+}
+
